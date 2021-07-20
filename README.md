da5873ac20506eaa6e09854b544c1c71a227e04f
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2021-07-20 14:58:36 -0300

    Squashing several commits so the HUB can start cleanly

--- README.md
+++ README.md
@@ -1,1 +1,149 @@
-# btfhub
+## BPF CO-RE programs and BTF files
+
+Portable [BPF](https://www.kernel.org/doc/html/latest/bpf/index.html) programs, called
+[CO-RE](https://facebookmicrosites.github.io/bpf/blog/2020/02/19/bpf-portability-and-co-re.html)
+(Compile Once - Run Everywhere), are portable because all needed symbol rellocations are done by
+[libbpf](https://github.com/libbpf/libbpf) before loading the BPF programs into the running kernel.
+
+Running kernels need to have [BTF](https://www.kernel.org/doc/html/latest/bpf/btf.html) type format
+available during runtime in order to allow libbpf to know how to do the rellocations. For the recent
+kernels this information is available through `/sys/kernel/btf/vmlinux` file:
+
+```bpftool btf dump file /sys/kernel/btf/vmlinux format c```
+
+as they have CONFIG_DEBUG_INFO_BTF option enabled, creating this sysfs interface.
+
+Older kernels don't provide this sysfs BTF interface, but are still capable of running BPF binaries.
+One of the things preventing older kernels to have CO-RE BPF binaries to run is the lack of BTF type
+format.
+
+**This is exactly what this HUB provides you:** an API that will feed your CO-RE BPF program - based
+on the libbpf library - with the needed btf(s) file(s) to make it portable, now even in older
+kernels.
+
+## How is this possible ?
+
+You might be wondering, how is this possible... if the kernels did not have the BTF feature during
+compilation time ? Thanks to [@acme](https://github.com/acmel), the Linux Perf maintainer, and other
+contributors, we can convert [DWARF symbols](https://en.wikipedia.org/wiki/DWARF) - available from
+existing kernel binary packages debug symbols - into a BTF only binary files.
+
+By using pahole, you may add an `.BTF` ELF section to the vmlinux kernel by doing:
+
+```
+pahole -J vmlinux
+```
+
+And then, with LLVM 10 or newer, you can extract the .BTF section into another ELF file:
+
+```
+llvm-objcopy-10 --only-section=.BTF --set-section-flags .BTF=alloc,readonly vmlinux vmlinux.btf
+```
+
+The result will be a 'vmlinux.btf' file containing BTF only ELF section that will feed libbpf for
+the needed symbols rellocations in that kernel. This means that you can create one BPF binary and
+run in all existing (and supported) kernels: new ones, because they have BTF data embedded, and old
+ones, because this HUB will provide you BTF data you need.
+
+> This HUB can only provide BTF files for distributions that already provide debug symbols for
+> kernel binary packages.
+
+### Recent Pahole Update
+
+Newer pahole (dwarves project) tool versions are capable of extracting BTF data from DWARF symbols
+into RAW files (instead of ELF files containing .BTF section). Those RAW files are capable of being
+used by libbpf so the CO-RE BPF relocations can be made.
+
+## Supported Kernels and Distribution Versions
+
+From this HUB you will always find the latest kernel patched version for the supported distribution.
+If your installation is outdated, then y our kernel might have been already replaced within your
+distribution repository. It is recommended that you upgrade to latest kernel version (patch version)
+for the Linux Distribution (and version) you are using.
+
+> `BTF = Y` -- The kernel already has support for BTF (no files from this HUB are needed).
+
+> `BTF = -` and `HUB = Y` -- The kernel does not support BTF but is capable of BPF. External files
+> from this HUB are needed and btfhub.io API will provide you with needed BTF file.
+
+### [Alma](https://en.wikipedia.org/wiki/AlmaLinux)
+
+| Alma Ver  | RHEL | Release Date  | RHEL Date  | Kernel     | BTF | HUB |
+|-----------|------|---------------|------------|------------|-----|-----|
+| 8.3       | 8.3  | 2021-03-30    | 2020-11-03 | 4.18.0-240 | Y   | -   |
+| 8.4       | 8.4  | 2021-05-26    | 2021-05-18 | 4.18.0-305 | Y   | -   |
+| ...       | ...  | ...           | ...        | ...        | Y   | -   |
+
+> **Note**: All kernels from Alma Linux releases have BTF support enabled
+
+### [CentOS](https://en.wikipedia.org/wiki/CentOS)
+
+##### CentOS 7 (and 7 Plus)
+
+| Centos Ver | RHEL | Release Date | RHEL Date  | Kernel      | BTF | HUB |
+|------------|------|--------------|------------|-------------|-----|-----|
+| 7.0.1406   | 7.0  | 2014-07      | 2014-06-09 | 3.10.0-123  | -   | Y   |
+| 7.1.1503   | 7.1  | 2015-03      | 2015-03-05 | 3.10.0-229  | -   | Y   |
+| 7.2.1511   | 7.2  | 2015-11      | 2015-11-19 | 3.10.0-327  | -   | Y   |
+| 7.3.1611   | 7.3  | 2016-11      | 2016-11-03 | 3.10.0-514  | -   | Y   |
+| 7.4.1708   | 7.4  | 2017-08      | 2017-07-31 | 3.10.0-693  | -   | Y   |
+| 7.5.1804   | 7.5  | 2018-04      | 2018-04-10 | 3.10.0-862  | -   | Y   |
+| 7.6.1810   | 7.6  | 2018-10      | 2018-10-30 | 3.10.0-957  | -   | Y   |
+| 7.7.1908   | 7.7  | 2019-08      | 2019-08-06 | 3.10.0-1062 | -   | Y   |
+| 7.8.2003   | 7.8  | 2020-03      | 2020-03-31 | 3.10.0-1127 | -   | Y   |
+| 7.9.2009 * | 7.9  | 2020-09      | 2020-09-29 | 3.10.0-1160 | -   | Y   |
+
+##### CentOS 8 (and 8 Plus)
+
+| Centos Ver | RHEL | Release Date | RHEL Date  | Kernel      | BTF | HUB |
+|------------|------|--------------|------------|-------------|-----|-----|
+| 8.0.1905   | 8.0  | 2019-09-24   | 2019-05-07 | 4.18.0-80   | -   | Y   |
+| 8.1.1911   | 8.1  | 2020-01-15   | 2019-11-05 | 4.18.0-147  | -   | Y   |
+| 8.2.2004   | 8.2  | 2020-06-15   | 2020-04-28 | 4.18.0-193  | Y   | -   |
+| 8.3.2011   | 8.3  | 2020-12-07   | 2020-11-03 | 4.18.0-240  | Y   | -   |
+| 8.4.2105 * | 8.4  | 2021-06-03   | 2021-05-18 | 4.18.0-305  | Y   | -   |
+| ...        | ...  | ...          | ...        | ...         | Y   | -   |
+
+> **Note**: All next kernels from CentOS 8 releases will likely have BTF support enabled
+
+##### CentOS Stream 8
+
+| Stream Ver | RHEL | Release Date | RHEL Date  | Kernel      | BTF | HUB |
+|------------|------|--------------|------------|-------------|-----|-----|
+|            |      |              |            |             |     |     |
+
+> **Note**: All kernels from CentOS Stream 8 releases have BTF support enabled
+>
+> \* Latest Version
+
+### [Fedora](https://en.wikipedia.org/wiki/Fedora_version_history)
+
+### [Ubuntu](https://en.wikipedia.org/wiki/Ubuntu_version_history)
+
+| Ubuntu Ver | Num     | Release Date | Kernel  | BTF | HUB |
+|------------|---------|--------------|---------|-----|-----|
+| Bionic     | 18.04.2 | 2018-04-26   | 4.15.0  | -   | -   |
+| Bionic HWE | -       | -            | 5.4.0   | -   | Y   |
+| Focal      | 20.04.2 | 2020-04-23   | 5.4.0   | -   | Y   |
+| Focal HWE  | -       | -            | 5.8.0   | -   | Y   |
+| Groovy     | 20.10   | 2020-10-22   | 5.8.0   | Y   | -   |
+| Groovy HWE | 20.10   | -            | 5.11.0	| Y   | -   |
+| Hirsute *  | 21.04   | 2021-04-22   | 5.11.0	| Y   | -   |
+| ...        | ...     | ...          | ...     | Y   | -   |
+
+> \* Latest Version
+
+### Disclaimer
+
+All the BTF files and their content were built from the DWARF symbols of their correspondent debug
+symbols package, from each of the supported distribution, using
+[LLVM](https://github.com/llvm/llvm-project) and pahole tool from the [dwarves
+project](https://github.com/acmel/dwarves).
+
+> No manipulation of those binary files was made during the process and all the files responsible
+> for generating the binary files are available in this repository.
+
+Debug Symbols Repositories:
+
+1. [debuginfo.centos.org](http://debuginfo.centos.org/)
+2. [ddebs.ubuntu.com](http://ddebs.ubuntu.com/)
---
+++ update.sh
@@ -0,0 +1,366 @@
+#!/bin/bash
+
+##
+## This script IS SUPPOSED to be a big monolithic script.
+## Thats it: The tree should focus in arranging BTF data.
+##
+
+## Syntax: $0 [bionic|focal|centos{7,8}|fedora{29,30,31,32}]
+
+basedir=$(dirname $0)
+if [ "$basedir" == "." ]; then
+	basedir=$(pwd)
+fi
+
+##
+## HELPER FUNCTIONS
+##
+
+exiterr() {
+	echo "ERROR: $@"
+	exit 1
+}
+
+warn() {
+	echo "WARN: $@"
+}
+
+info() {
+	echo "INFO: $@"
+}
+
+###
+### 1. UBUNTU
+###
+
+### bionic (5.4 hwe kernels)
+
+[[ "$1" == "bionic" ]] && {
+
+origdir=$(pwd)
+repository="http://ddebs.ubuntu.com"
+regex="linux-image-unsigned-5\.4\..*-generic-dbgsym"
+
+mkdir -p $basedir/ubuntu/bionic
+cd $basedir/ubuntu/bionic || exiterr "no bionic dir found"
+
+wget http://ddebs.ubuntu.com/dists/bionic/main/binary-amd64/Packages -O bionic
+wget http://ddebs.ubuntu.com/dists/bionic-updates/main/binary-amd64/Packages -O bionic-updates
+
+[ ! -f bionic ] && exiterr "no bionic packages file found"
+[ ! -f bionic-updates ] && exiterr "no bionic-updates packages file found"
+
+cat bionic | grep -E '^(Package|Filename):' | grep --no-group-separator -A1 -E "^Package: $regex" > temp
+cat bionic-updates | grep -E '^(Package|Filename):' | grep --no-group-separator -A1 -E "Package: $regex" >> temp
+rm bionic; rm bionic-updates; mv temp packages
+
+# for kernel debug symbols packages in ubuntu bionic repository
+for package in $(cat packages | grep Package: | sed 's:Package\: ::g' | sort)
+do
+
+	# get filename and url to download file from
+	filepath=$(cat packages | grep -A1 $package | grep -v "^Package: " | sed 's:Filename\: ::g')
+	url=$(echo $repository/$filepath)
+	filename=$(basename $filepath)
+	version=$(echo $filename | cut -d'-' -f4,5,6)
+
+	echo URL: $url
+	echo FILEPATH: $filepath
+	echo FILENAME: $filename
+	echo VERSION: $version
+
+	# do not download dbg package again (if BTF file already exists)
+	if [ -f $version.btf.tar.xz ]
+	then
+		info "file $version.btf already exists"
+		continue
+	fi
+
+	# accelerate download if possible
+	axel -4 -n 6 $url
+	mv $filename $version.ddeb
+	if [ ! -f $version.ddeb ]
+	then
+		warn "$version.ddeb could not be downloaded"
+		continue
+	fi
+
+	# extract vmlinux file from ddeb package
+	dpkg --fsys-tarfile $version.ddeb | tar xvf - ./usr/lib/debug/boot/vmlinux-$version
+	mv ./usr/lib/debug/boot/vmlinux-$version ./$version.vmlinux
+	rm -rf ${basedir}/ubuntu/bionic/usr
+
+	# generate BTF raw file from DWARF data
+	pahole -j $version.btf $version.vmlinux
+	pahole $version.btf > $version.txt
+	tar cvfJ ./$version.btf.tar.xz $version.btf
+
+	rm $version.ddeb
+	rm $version.btf
+	rm $version.txt
+	rm $version.vmlinux
+
+done
+
+rm -f $basedir/ubuntu/bionic/packages
+cd $origdir >/dev/null
+
+}
+
+### focal (5.4 and 5.8 hwe kernels)
+
+[[ "$1" == "focal" ]] && {
+
+origdir=$(pwd)
+repository="http://ddebs.ubuntu.com"
+regex="linux-image-unsigned-5\.(4|8)\..*-generic-dbgsym"
+
+mkdir -p $basedir/ubuntu/focal
+cd $basedir/ubuntu/focal || exiterr "no focal dir found"
+
+wget http://ddebs.ubuntu.com/dists/focal/main/binary-amd64/Packages -O focal
+wget http://ddebs.ubuntu.com/dists/focal-updates/main/binary-amd64/Packages -O focal-updates
+
+[ ! -f focal ] && exiterr "no focal packages file found"
+[ ! -f focal-updates ] && exiterr "no focal-updates packages file found"
+
+cat focal | grep -E '^(Package|Filename):' | grep --no-group-separator -A1 -E "^Package: $regex" > temp
+cat focal-updates | grep -E '^(Package|Filename):' | grep --no-group-separator -A1 -E "Package: $regex" >> temp
+rm focal; rm focal-updates; mv temp packages
+
+# for kernel debug symbols packages in ubuntu focal repository
+for package in $(cat packages | grep Package: | sed 's:Package\: ::g' | sort)
+do
+
+	# get filename and url to download file from
+	filepath=$(cat packages | grep -A1 $package | grep -v "^Package: " | sed 's:Filename\: ::g')
+	url=$(echo $repository/$filepath)
+	filename=$(basename $filepath)
+	version=$(echo $filename | cut -d'-' -f4,5,6)
+
+	echo URL: $url
+	echo FILEPATH: $filepath
+	echo FILENAME: $filename
+	echo VERSION: $version
+
+	# do not download dbg package again (if BTF file already exists)
+	if [ -f $version.btf.tar.xz ]
+	then
+		info "file $version.btf already exists"
+		continue
+	fi
+
+	# accelerate download if possible
+	axel -4 -n 6 $url
+	mv $filename $version.ddeb
+	if [ ! -f $version.ddeb ]
+	then
+		warn "$version.ddeb could not be downloaded"
+		continue
+	fi
+
+	# extract vmlinux file from ddeb package
+	dpkg --fsys-tarfile $version.ddeb | tar xvf - ./usr/lib/debug/boot/vmlinux-$version
+	mv ./usr/lib/debug/boot/vmlinux-$version ./$version.vmlinux
+	rm -rf ${basedir}/ubuntu/focal/usr
+
+	# generate BTF raw file from DWARF data
+	pahole -j $version.btf $version.vmlinux
+	pahole $version.btf > $version.txt
+	tar cvfJ ./$version.btf.tar.xz $version.btf
+
+	rm $version.ddeb
+	rm $version.btf
+	rm $version.txt
+	rm $version.vmlinux
+
+done
+
+rm -f $basedir/ubuntu/focal/packages
+cd $origdir >/dev/null
+
+}
+
+###
+### 2. CENTOS
+###
+
+### centos7 and centos8
+
+[[ "$1" == centos* ]] && {
+
+centosrel=$1
+centosver=${1/centos/}
+origdir=$(pwd)
+
+case $centosver in
+
+7)
+  # end of life version
+  repository="http://mirror.facebook.net/centos-debuginfo/7/x86_64/"
+  ;;
+8)
+  # current version
+  repository="http://mirror.facebook.net/centos-debuginfo/8/x86_64/Packages/"
+  ;;
+*)
+  exiterr "only centos7 and centos8 are supported"
+esac
+
+regex="kernel-debug-debuginfo.*x86_64.rpm"
+
+mkdir -p $basedir/centos/$centosrel
+cd $basedir/centos/$centosrel || exiterr "no $centosrel dir found"
+
+info "downloading $repository information"
+lynx -dump -listonly $repository | tail -n+4 > $centosrel
+[[ ! -f $centosrel ]] && exiterr "no $centosrel packages file found"
+cat $centosrel | grep -E $regex | awk '{print $2}' > temp
+mv temp packages
+rm $centosrel
+
+# for kernel debug symbols packages in $centosrel repository
+for line in $(cat packages | sort)
+do
+	# get filename and url to download file from
+	url=$line
+	dirname=$(dirname $line)
+	filename=$(basename $line)
+	version=$(echo $filename | cut -d'-' -f4,5,6,7,8,9 | sed 's:.x86_64.rpm::g')
+
+	echo URL: $url
+	echo FILENAME: $filename
+	echo VERSION: $version
+
+	# do not download dbg package again (if BTF file already exists)
+	if [ -f $version.btf.tar.xz ]
+	then
+		info "file $version.btf already exists"
+		continue
+	fi
+
+	# accelerate download if possible
+	axel -4 -n 8 $url
+	mv $filename $version.rpm
+	if [ ! -f $version.rpm ]
+	then
+		warn "$version.rpm could not be downloaded"
+		continue
+	fi
+
+	# extract vmlinux file from rpm package
+	vmlinux=".$(rpmquery -qlp $version.rpm 2>&1 | grep vmlinux)"
+	echo "INFO: extracting vmlinux from: $version.rpm"
+	rpm2cpio $version.rpm | cpio --to-stdout -i $vmlinux > ./$version.vmlinux
+
+	# generate BTF raw file from DWARF data
+	echo "INFO: generating BTF file: $version.btf"
+	pahole -j $version.btf $version.vmlinux
+	pahole $version.btf > $version.txt
+	tar cvfJ ./$version.btf.tar.xz $version.btf
+
+	rm $version.rpm
+	rm $version.btf
+	rm $version.txt
+	rm $version.vmlinux
+done
+
+rm -f $basedir/centos/$centosrel/packages
+cd $origdir >/dev/null
+
+}
+
+###
+### 3. Fedora
+###
+
+### fedora29, fedora30, fedora31 and fedora32
+
+[[ "$1" == fedora* ]] && {
+
+fedorarel=$1
+fedoraver=${1/fedora/}
+origdir=$(pwd)
+
+case $fedoraver in
+
+29 | 30 | 31)
+  # end of life versions
+  repository01="https://archives.fedoraproject.org/pub/archive/fedora/linux/releases/$fedoraver/Everything/x86_64/debug/tree/Packages/k/"
+  repository02="https://archives.fedoraproject.org/pub/archive/fedora/linux/updates/$fedoraver/Everything/x86_64/debug/Packages/k/"
+  ;;
+32)
+  # intermediate ? there is no updates url available and archives.fedoraproject.org does not have fedora32 available (yet ?)
+  repository01="https://dl.fedoraproject.org/pub/fedora/linux/releases/32/Everything/x86_64/debug/tree/Packages/k/"
+  repository02="https://dl.fedoraproject.org/pub/fedora/linux/releases/32/Everything/x86_64/debug/tree/Packages/k/"
+  ;;
+*)
+  exiterr "only fedora29, fedora30, fedora31 and fedora32 are supported"
+esac
+
+regex="kernel-debug-debuginfo.*x86_64.rpm"
+
+mkdir -p $basedir/fedora/$fedorarel
+cd $basedir/fedora/$fedorarel || exiterr "no $fedorarel dir found"
+
+info "downloading $repository01 information"
+lynx -dump -listonly $repository01 | tail -n+4 > $fedorarel
+info "downloading $repository02 information"
+lynx -dump -listonly $repository02 | tail -n+4 >> $fedorarel
+[[ ! -f $fedorarel ]] && exiterr "no $fedorarel packages file found"
+cat $fedorarel | grep -E $regex | awk '{print $2}' > temp
+mv temp packages
+rm $fedorarel
+
+# for kernel debug symbols packages in $fedorarel repository
+for line in $(cat packages | sort)
+do
+	# get filename and url to download file from
+	url=$line
+	dirname=$(dirname $line)
+	filename=$(basename $line)
+	version=$(echo $filename | cut -d'-' -f4,5,6,7,8,9 | sed 's:.x86_64.rpm::g')
+
+	echo URL: $url
+	echo FILENAME: $filename
+	echo VERSION: $version
+
+	# do not download dbg package again (if BTF file already exists)
+	if [ -f $version.btf.tar.xz ]
+	then
+		info "file $version.btf already exists"
+		continue
+	fi
+
+	# accelerate download if possible
+	axel -4 -n 8 $url
+	mv $filename $version.rpm
+	if [ ! -f $version.rpm ]
+	then
+		warn "$version.rpm could not be downloaded"
+		continue
+	fi
+
+	# extract vmlinux file from rpm package
+	vmlinux=".$(rpmquery -qlp $version.rpm 2>&1 | grep vmlinux)"
+	echo "INFO: extracting vmlinux from: $version.rpm"
+	rpm2cpio $version.rpm | cpio --to-stdout -i $vmlinux > ./$version.vmlinux
+
+	# generate BTF raw file from DWARF data
+	echo "INFO: generating BTF file: $version.btf"
+	pahole -j $version.btf $version.vmlinux
+	pahole $version.btf > $version.txt
+	tar cvfJ ./$version.btf.tar.xz $version.btf
+
+	rm $version.rpm
+	rm $version.btf
+	rm $version.txt
+	rm $version.vmlinux
+done
+
+rm -f $basedir/fedora/$fedorarel/packages
+cd $origdir >/dev/null
+
+}
+
+exit 0
