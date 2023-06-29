92a1d49cf1199084c35ce9c8b0eb5c0afb03377b
Bryce Kahle <bryce.kahle@datadoghq.com>
2023-06-29 12:26:57 -0700

    Update pahole flags to match kernel
    See https://github.com/torvalds/linux/blob/master/scripts/pahole-flags.sh

--- pkg/job/utils.go
+++ pkg/job/utils.go
@@ -8,5 +8,5 @@ import (
 
 // GenerateBTF generates a BTF file from a vmlinux file
 func GenerateBTF(ctx context.Context, vmlinux string, out string) error {
-	return utils.RunCMD(ctx, "", "pahole", "--btf_encode_detached", out, vmlinux)
+	return utils.RunCMD(ctx, "", "pahole", "--btf_gen_floats", "--skip_encoding_btf_inconsistent_proto", "--btf_gen_optimized", "--btf_encode_detached", out, vmlinux)
 }
