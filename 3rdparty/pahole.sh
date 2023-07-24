fe155c18072c5c1705024a457e987dbab14bdceb
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2023-07-24 12:18:40 -0300

    fix(build): Install pahole from the source (#95)

---
+++ 3rdparty/pahole.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+git submodule update --init
+export DEBIAN_FRONTEND=noninteractive
+rm -f *.deb
+cd ./3rdparty/dwarves
+rm -rf ./debian/
+cp -rfp ../debian.dwarves/ ./debian/
+sudo apt-get -y install devscripts ubuntu-dev-tools
+sudo apt-get -y build-dep .
+fakeroot dh binary
+rm -rf ./debian/
+cd ..
+sudo dpkg -i ./pahole*.deb
+rm -f *.deb
